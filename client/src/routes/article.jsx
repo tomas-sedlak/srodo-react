1b2d49c6cb7df15d2b5ff9f34eb352c07064c9b1
mark-takac <steam.omg.mk@gmail.com>
2024-01-11 00:09:32 +0100

    Added comments design

--- client/src/routes/article.jsx
+++ client/src/routes/article.jsx
@@ -1,6 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useLoaderData } from "react-router-dom";
-import { AspectRatio, Box, Image, Text, Group, Title, TypographyStylesProvider, Avatar } from '@mantine/core';
+import { AspectRatio, Box, Image, Text, Group, Title, TypographyStylesProvider, Avatar, TextInput, ActionIcon, Flex, Paper, UnstyledButton, Collapse } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconArrowRight } from '@tabler/icons-react';
 import { Link } from "react-router-dom";
 
 import moment from "moment";
@@ -21,6 +23,8 @@ export default function Article() {
             .then(result => result.json())
             .then(data => setUser(data))
     }, [])
+
+    const [openned, { toggle }] = useDisclosure(false)
 
     return (
         <>
@@ -57,6 +61,71 @@ export default function Article() {
             </Box>
 
             {/* TODO: add comment section */}
+
+            <Box p="sm">
+
+                {/* Adding new comments */}
+
+                <Flex w="100%" gap="sm" pb="sm">
+                    <Avatar />
+                    <TextInput w="100%" placeholder="Tu mi napíš niečo pekné" radius="lg" rightSection={
+                        <ActionIcon radius="lg" variant="default">
+                            <IconArrowRight stroke={1.25} />
+                        </ActionIcon>
+                    } 
+                    />
+                </Flex>
+
+                {/* Comment head section, displayname etc. */}
+
+                <Group gap={4} align="center" mt="sm" >
+                    <Avatar size="sm" />
+
+                    <Link to="username">
+                        <Text fw={600} c="gray" size="sm">
+                            Display name
+                        </Text>
+                    </Link>
+                    <Text c="gray" size="sm">
+                        &middot; pred 13 min
+                    </Text>
+                </Group>
+
+                {/* The comment itself and the answer button */}
+
+                    <Text size="sm" className="Comment" p="sm">Awesome comment</Text> {/* This is NOT how the border should look, it NEEDS a BETTER solution */}
+                    <UnstyledButton className="Text-button" c="gray" ml="sm" onClick={toggle}>Odpovedať</UnstyledButton>
+
+                {/* Answers to comments */}
+
+                <Collapse in={openned} pt="sm" ml="md">
+
+                    <Flex w="100%" gap="sm" pb="sm">
+                        <Avatar />
+                        <TextInput w="100%" placeholder="Tu mi napíš niečo pekné" radius="lg" rightSection={
+                            <ActionIcon radius="lg" variant="default">
+                                <IconArrowRight stroke={1.25} />
+                            </ActionIcon>
+                            // I think it looks good with the icon but it might be changed later
+                        } 
+                        />
+                    </Flex>
+
+                    <Group gap={4} align="center" mt="sm">
+                        <Avatar size="sm" />
+
+                        <Link to="username">
+                            <Text fw={600} c="gray" size="sm">
+                                Display name
+                            </Text>
+                        </Link>
+                        <Text c="gray" size="sm">
+                            &middot; pred 13 min
+                        </Text>
+                    </Group>
+                    <Text size="sm" className="Comment" p="sm">No, it's not awesome. It should be Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatem ab optio, atque fuga iure doloremque quasi impedit reprehenderit aut inventore aspernatur! Voluptates quos recusandae obcaecati provident nostrum delectus laboriosam! Ipsam.</Text>
+                </Collapse>
+            </Box>
         </>
     )
 }
