0d9fb47daa5ea086ff0fe206a5e00a31ec953a34
tomassedlak <t.sedlak.24@gmail.com>
2024-02-13 21:30:48 +0100

    Upvotes/downvotes works on frontend but withour refresh

--- client/src/templates/Comment.jsx
+++ client/src/templates/Comment.jsx
@@ -1,12 +1,31 @@
 import { Box, Text, Group, Avatar, TypographyStylesProvider } from '@mantine/core';
-import { IconArrowBigUp, IconArrowBigDown } from '@tabler/icons-react';
+import { IconArrowBigUp, IconArrowBigUpFilled, IconArrowBigDown, IconArrowBigDownFilled } from '@tabler/icons-react';
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 import moment from "moment";
 import "moment/dist/locale/sk";
 moment.locale("sk");
 
 export default function Comment({ data }) {
+    const userId = useSelector(state => state.user._id);
+    const token = useSelector(state => state.token);
+
+    const upvote = async () => {
+        await axios.patch(`/api/comment/${data._id}/upvote`,
+            { userId },
+            { headers: { Authorization: `Bearer ${token}` } }
+        );
+    }
+
+    const downvote = async () => {
+        await axios.patch(`/api/comment/${data._id}/downvote`,
+            { userId },
+            { headers: { Authorization: `Bearer ${token}` } }
+        );
+    }
+
     return (
         <Box mt={8}>
             <Group gap={8}>
@@ -30,9 +49,15 @@ export default function Comment({ data }) {
 
             <Group ml={46} gap={8}>
                 <div className="icon-wrapper">
-                    <IconArrowBigUp stroke={1.25} />
+                    {!data.upvotes.includes(userId) ?
+                        <IconArrowBigUp stroke={1.25} onClick={upvote} />
+                        : <IconArrowBigUpFilled stroke={1.25} onClick={upvote} />
+                    }
                     <span>{data.upvotes.length - data.downvotes.length}</span>
-                    <IconArrowBigDown stroke={1.25} />
+                    {!data.downvotes.includes(userId) ?
+                        <IconArrowBigDown stroke={1.25} onClick={downvote} />
+                        : <IconArrowBigDownFilled stroke={1.25} onClick={downvote} />
+                    }
                 </div>
             </Group>
         </Box>
