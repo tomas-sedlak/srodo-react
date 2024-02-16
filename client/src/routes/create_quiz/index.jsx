81cc432295f71fd70d683d303c30d75ddf90d3a8
mark-takac <steam.omg.mk@gmail.com>
2024-02-16 22:30:17 +0100

    Small changes on quiz frontend

--- client/src/routes/create_quiz/index.jsx
+++ client/src/routes/create_quiz/index.jsx
@@ -1,3 +1,107 @@
+import { Button, Box, AspectRatio, Image as MantineImage, Flex, Text, Card, Checkbox, ActionIcon, Container, TextInput, UnstyledButton, Group } from "@mantine/core";
+import StarterKit from "@tiptap/starter-kit";
+import { RichTextEditor } from "@mantine/tiptap";
+import { useEditor } from "@tiptap/react";
+import Placeholder from '@tiptap/extension-placeholder';
+import { IconPhoto, IconPlus } from "@tabler/icons-react";
+import ImagesModal from "templates/ImagesModal";
+import { useEffect, useState } from "react";
+import { createClient } from 'pexels';
+import { useDisclosure } from "@mantine/hooks";
+import axios from "axios";
+
+
+
 export default function CreateQuiz() {
-    return (<></>)
+
+    useEffect(() => {
+        client.photos.curated({ per_page: 1, page: 1 }).then(
+            response => setCoverImage(response.photos[0].src.landscape)
+        )
+    });
+
+    const client = createClient('prpnbgyqErzVNroSovGlQyX5Z1Ybl8z3hAEhaingf99gTztS33sMZwg1');
+    const [coverImage, setCoverImage] = useState("");
+    const [coverImageModalOpened, coverImageModalHandlers] = useDisclosure(false);
+    const [imageModalOpened, imageModalHandlers] = useDisclosure(false);
+
+    const addImage = url => {
+        editor.chain().focus().setImage({ src: url }).run()
+    }
+
+    const editor = useEditor({
+        extensions: [
+            StarterKit,
+            Placeholder.configure({ placeholder: "Napíš otázku" })
+        ],
+        content: ""
+    })
+
+    return (
+        <>
+            {/* Pictures  */}
+
+            <ImagesModal opened={coverImageModalOpened} close={coverImageModalHandlers.close} setImage={setCoverImage} />
+            <ImagesModal opened={imageModalOpened} close={imageModalHandlers.close} setImage={addImage} />
+
+            {/* <Button fullWidth radius="md" variant="outline" leftSection={<IconPhoto stroke={1.25} />} >
+            Add photo
+        </Button> */}
+            <Box p="sm" >
+                <Box pos="relative">
+                    <AspectRatio ratio={2 / 1}>
+                        <MantineImage onClick={coverImageModalHandlers.open} className="pointer" radius="lg" src={coverImage} />
+                    </AspectRatio>
+                </Box>
+                <Flex mt="sm" gap="sm" align="center" >
+                    <RichTextEditor
+                        editor={editor}
+                        style={{ flex: 1 }}
+                    >
+                        <RichTextEditor.Content />
+                    </RichTextEditor>
+                    <Button>
+                        Pridať otázku
+                    </Button>
+                </Flex>
+                <Card mt="sm" withBorder mb="md" >
+                    <Text fw="bold" >Lorem ipsum dolor, sit amet consectetur adipisicing elit.</Text>
+                    <>
+                        <Flex align="center" gap="sm" p="sm" >
+                            <Checkbox />
+                            <TextInput placeholder="Add answer" w="100%" />
+                        </Flex>
+                        <Flex align="center" gap="sm" p="sm" >
+                            <Checkbox />
+                            <TextInput placeholder="Add answer" w="100%" />
+                        </Flex>
+                        <Flex align="center" gap="sm" p="sm" >
+                            <Checkbox />
+                            <TextInput placeholder="Add answer" w="100%" />
+                        </Flex>
+                        <Flex align="center" gap="sm" p="sm" >
+                            <Button
+                                variant="subtle"
+                                leftSection={<IconPlus stroke={1.25} />}
+                                c="black"
+                                color="gray"
+                                pl={0}
+                            >
+                                Add a question
+                            </Button>
+                        </Flex>
+                    </>
+
+                </Card>
+                <Group gap="lg" grow align="center" >
+                    <Button variant="light" color="black">Save as draft</Button>
+                    <Button variant="light" color="black">Publish</Button>
+                    <Button variant="light" color="black">Add a picture</Button>
+                   
+                </Group>
+
+            </Box>
+
+        </>
+    )
 }
--- client/src/templates/ImagesModal.jsx
+++ client/src/templates/ImagesModal.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Loader, Text, Image, TextInput, Modal, CloseButton, Box } from '@mantine/core';
-import { IconSearch } from '@tabler/icons-react';
+import { IconSearch,IconX, IconUpload, IconPhoto } from '@tabler/icons-react';
 import { useMediaQuery } from '@mantine/hooks';
 import { createClient } from 'pexels';
 import { useInfiniteQuery } from "@tanstack/react-query";
@@ -170,7 +170,44 @@ export default function ImagesModal({ opened, close, setImage }) {
                     ) : tab === "url" ? (
                         <Text>Nahraj z URL</Text>
                     ) : (
-                        <Text>Zo zariadenia</Text>
+                        // Trying the dropzone
+
+                        <Dropzone
+                            onDrop={(files) => console.log('accepted files', files)}
+                            onReject={(files) => console.log('rejected files', files)}
+                            maxSize={5 * 1024 ** 2}
+                            accept={IMAGE_MIME_TYPE}
+                        >
+                            <Group justify="center" gap="xl" mih={220} style={{ pointerEvents: 'none' }}>
+                                <Dropzone.Accept>
+                                    <IconUpload
+                                        style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
+                                        stroke={1.5}
+                                    />
+                                </Dropzone.Accept>
+                                <Dropzone.Reject>
+                                    <IconX
+                                        style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
+                                        stroke={1.5}
+                                    />
+                                </Dropzone.Reject>
+                                <Dropzone.Idle>
+                                    <IconPhoto
+                                        style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
+                                        stroke={1.5}
+                                    />
+                                </Dropzone.Idle>
+
+                                <div>
+                                    <Text size="xl" inline>
+                                        Drag images here or click to select files
+                                    </Text>
+                                    <Text size="sm" c="dimmed" inline mt={7}>
+                                        Attach as many files as you like, each file should not exceed 5mb
+                                    </Text>
+                                </div>
+                            </Group>
+                        </Dropzone>
                     )}
                 </Modal.Body>
             </Modal.Content>
