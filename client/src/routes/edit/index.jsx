a7003043d7aa9c4301f668756d3ed43dbcfe4750
tomassedlak <t.sedlak.24@gmail.com>
2024-02-19 23:25:10 +0100

    Added fetching to edit page

--- client/src/routes/edit/index.jsx
+++ client/src/routes/edit/index.jsx
@@ -1,7 +1,30 @@
+import { useParams } from "react-router-dom";
+import { useQuery } from "@tanstack/react-query"
+import { Loader } from "@mantine/core";
+import axios from "axios"
+
 export default function Edit() {
-    return (
-        <p>
-            Upravit prispevok
-        </p>
+    const { postId } = useParams();
+
+    const fetchPost = async () => {
+        const result = await axios.get(`/api/post/${postId}`)
+        return result.data
+    }
+
+    const { data, status } = useQuery({
+        queryKey: ["editPost"],
+        queryFn: fetchPost
+    })
+
+    return status === "pending" ? (
+        <div className="loader-center-x">
+            <Loader />
+        </div>
+    ) : status === "error" ? (
+        <div className="loader-center">
+            <p>Nastala chyba!</p>
+        </div>
+    ) : (
+        <></>
     )
 }
