d0b70382241e392b2518557f63c80e743ecf4526
mark-takac <steam.omg.mk@gmail.com>
2024-02-24 14:38:47 +0100

    Added working quiz editor, img not working

--- client/src/routes/create_quiz/index.jsx
+++ client/src/routes/create_quiz/index.jsx
@@ -1,9 +1,4 @@
-import { Button, Box, AspectRatio, Image as MantineImage, Flex, Text, Card, Checkbox, ActionIcon, Container, TextInput, UnstyledButton, Group } from "@mantine/core";
-import StarterKit from "@tiptap/starter-kit";
-import { RichTextEditor } from "@mantine/tiptap";
-import { useEditor } from "@tiptap/react";
-import Placeholder from '@tiptap/extension-placeholder';
-import { IconPhoto, IconPlus } from "@tabler/icons-react";
+import { Button, Box, AspectRatio, Image as MantineImage, Flex, Card, Checkbox, TextInput, Group } from "@mantine/core";
 import ImagesModal from "templates/ImagesModal";
 import { useEffect, useState } from "react";
 import { createClient } from 'pexels';
@@ -21,87 +16,106 @@ export default function CreateQuiz() {
     });
 
     const client = createClient('prpnbgyqErzVNroSovGlQyX5Z1Ybl8z3hAEhaingf99gTztS33sMZwg1');
+
     const [coverImage, setCoverImage] = useState("");
     const [coverImageModalOpened, coverImageModalHandlers] = useDisclosure(false);
     const [imageModalOpened, imageModalHandlers] = useDisclosure(false);
 
-    const addImage = url => {
-        editor.chain().focus().setImage({ src: url }).run()
-    }
 
-    const editor = useEditor({
-        extensions: [
-            StarterKit,
-            Placeholder.configure({ placeholder: "Napíš otázku" })
-        ],
-        content: ""
-    })
+    const [questions, setQuestions] = useState([{ question: '', options: [] }]);
+
+    const handleQuestionChange = (index, value) => {
+        const newQuestions = [...questions];
+        newQuestions[index].question = value;
+        setQuestions(newQuestions);
+    };
+
+    const handleOptionChange = (questionIndex, optionIndex, value) => {
+        const newQuestions = [...questions];
+        newQuestions[questionIndex].options[optionIndex] = value;
+        setQuestions(newQuestions);
+    };
+
+    const handleAddOption = (index) => {
+        const newQuestions = [...questions];
+        newQuestions[index].options.push('');
+        setQuestions(newQuestions);
+    };
+
+    const handleAddQuestion = () => {
+        setQuestions([...questions, { question: '', options: [] }]);
+    };
+
 
     return (
         <>
-            {/* Pictures  */}
-
             <ImagesModal opened={coverImageModalOpened} close={coverImageModalHandlers.close} setImage={setCoverImage} />
-            <ImagesModal opened={imageModalOpened} close={imageModalHandlers.close} setImage={addImage} />
+            <ImagesModal opened={imageModalOpened} close={imageModalHandlers.close} />
 
-            {/* <Button fullWidth radius="md" variant="outline" leftSection={<IconPhoto stroke={1.25} />} >
-            Add photo
-        </Button> */}
-            <Box p="sm" >
-                <Box pos="relative">
-                    <AspectRatio ratio={2 / 1}>
-                        <MantineImage onClick={coverImageModalHandlers.open} className="pointer" radius="lg" src={coverImage} />
-                    </AspectRatio>
-                </Box>
-                <Flex mt="sm" gap="sm" align="center" >
-                    <RichTextEditor
-                        editor={editor}
-                        style={{ flex: 1 }}
+            {questions.map((question, questionIndex) => (
+                <Card key={questionIndex} mt={questionIndex > 0 ? 'sm' : 0} withBorder mb="md">
+                    <Box pos="relative" pb="sm">
+                        <AspectRatio ratio={2 / 1}>
+                            <Box
+                                className="lazy-image pointer"
+                                style={{ backgroundImage: `url(${coverImage})` }}
+                                onClick={coverImageModalHandlers.open}
+                            ></Box>
+                        </AspectRatio>
+                    </Box>
+                    <TextInput
+                        fw="bold"
+                        placeholder="Ask a question"
+                        value={question.question}
+                        onChange={(event) => handleQuestionChange(questionIndex, event.target.value)}
+                    />
+
+                    {question.options.map((option, optionIndex) => (
+                        <Flex key={optionIndex} align="center" gap="sm" p="sm">
+                            <Checkbox />
+                            <TextInput
+                                placeholder="Add answer"
+                                value={option}
+                                onChange={(event) =>
+                                    handleOptionChange(questionIndex, optionIndex, event.target.value)
+                                }
+                            />
+                        </Flex>
+                    ))}
+
+                    <Button
+                        variant="subtle"
+                        c="black"
+                        color="gray"
+                        onClick={() => handleAddOption(questionIndex)}
                     >
-                        <RichTextEditor.Content />
-                    </RichTextEditor>
-                    <Button>
-                        Pridať otázku
+                        Add an answer
                     </Button>
-                </Flex>
-                <Card mt="sm" withBorder mb="md" >
-                    <Text fw="bold" >Lorem ipsum dolor, sit amet consectetur adipisicing elit.</Text>
-                    <>
-                        <Flex align="center" gap="sm" p="sm" >
-                            <Checkbox />
-                            <TextInput placeholder="Add answer" w="100%" />
-                        </Flex>
-                        <Flex align="center" gap="sm" p="sm" >
-                            <Checkbox />
-                            <TextInput placeholder="Add answer" w="100%" />
-                        </Flex>
-                        <Flex align="center" gap="sm" p="sm" >
-                            <Checkbox />
-                            <TextInput placeholder="Add answer" w="100%" />
-                        </Flex>
-                        <Flex align="center" gap="sm" p="sm" >
-                            <Button
-                                variant="subtle"
-                                leftSection={<IconPlus stroke={1.25} />}
-                                c="black"
-                                color="gray"
-                                pl={0}
-                            >
-                                Add a question
-                            </Button>
-                        </Flex>
-                    </>
+                    <Group grow>
+                        <Button
+                            mt="sm"
+                            onClick={handleAddQuestion}
+                        >
+                            Add another question
+                        </Button>
+                        <Button
+                            mt="sm"
+                            onClick={imageModalHandlers.open}
+                        >
+                            Add a picture
+                        </Button>
+                        <Button
+                            mt="sm"
+                        >
+                            Publish this quiz
+                        </Button>
+                    </Group>
 
                 </Card>
-                <Group gap="lg" grow align="center" >
-                    <Button variant="light" color="black">Save as draft</Button>
-                    <Button variant="light" color="black">Publish</Button>
-                    <Button variant="light" color="black">Add a picture</Button>
-                   
-                </Group>
+            ))}
 
-            </Box>
 
         </>
     )
+
 }
