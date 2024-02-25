e16da891cc6bdd053cbdd9cef38deddf5eb7b371
tomassedlak <t.sedlak.24@gmail.com>
2024-02-25 22:19:00 +0100

    Added working delete comment

--- routes/comment.js
+++ routes/comment.js
@@ -2,13 +2,17 @@ import express from "express";
 import {
   upvoteComment,
   downvoteComment,
+  deleteComment,
 } from "../controllers/comment.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
-/* UPDATE */
+// UPDATE
 router.patch("/:commentId/upvote", verifyToken, upvoteComment);
 router.patch("/:commentId/downvote", verifyToken, downvoteComment);
+
+// DELETE
+router.delete("/:commentId", verifyToken, deleteComment);
 
 export default router;
