346f36d7142d117895b9e1933d9074cae1110889
tomassedlak <t.sedlak.24@gmail.com>
2024-02-29 12:49:16 +0100

    Prevention from comment spams

--- client/src/templates/Comments.jsx
+++ client/src/templates/Comments.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Group, Button, Loader, Text } from '@mantine/core';
+import { Box, Group, Button, Loader } from '@mantine/core';
 import { useSelector, useDispatch } from "react-redux";
 import { setLoginModal } from "state";
 import { useQuery } from "@tanstack/react-query";
@@ -8,30 +8,36 @@ import { useCurrentEditor } from "@tiptap/react";
 import Comment from "templates/Comment";
 import axios from "axios";
 
-const PublishButton = ({ postId, text, refetch }) => {
+const PublishButton = ({ postId, refetch }) => {
     const { editor } = useCurrentEditor()
     const userId = useSelector(state => state.user?._id);
     const token = useSelector(state => state.token);
     const dispatch = useDispatch();
+    const [isPublishing, setIsPublishing] = useState(false);
 
     return (
         <Group px="sm" pb={8} justify="flex-end">
             <Button
+                disabled={isPublishing}
                 onClick={async () => {
                     if (userId) {
-                        // Check if not empty
-                        if (text.trim() !== "") {
-                            await axios.post(`/api/post/${postId}/comment`, {
-                                postId: postId,
-                                author: userId,
-                                content: text.trim(),
-                            }, {
-                                headers: { Authorization: `Bearer ${token}` }
-                            })
+                        // Check if empty
+                        if (editor.getText() === "") return
 
-                            editor.commands.clearContent()
-                            refetch()
-                        }
+                        setIsPublishing(true)
+
+                        await axios.post(`/api/post/${postId}/comment`, {
+                            postId: postId,
+                            author: userId,
+                            content: editor.getHTML(),
+                        }, {
+                            headers: { Authorization: `Bearer ${token}` }
+                        })
+                        
+                        editor.commands.clearContent()
+                        refetch()
+
+                        setIsPublishing(false)
                     } else {
                         dispatch(setLoginModal(true))
                     }
@@ -44,8 +50,6 @@ const PublishButton = ({ postId, text, refetch }) => {
 }
 
 export default function Comments({ postId }) {
-    const [text, setText] = useState("");
-
     const fetchComments = async () => {
         const response = await axios.get(`/api/post/${postId}/comments`);
         return response.data;
@@ -68,11 +72,10 @@ export default function Comments({ postId }) {
         <>
             <Box p="sm" className="border-bottom">
                 <TextEditor
-                    setText={setText}
                     placeholder="Napíš komentár"
                     simple
                     slotAfter={
-                        <PublishButton postId={postId} text={text} refetch={refetch} />
+                        <PublishButton postId={postId} refetch={refetch} />
                     }
                 />
             </Box>
