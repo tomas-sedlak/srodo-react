3dda9453aa4f74966336143542ca4fe83fa1310c
tomassedlak <t.sedlak.24@gmail.com>
2024-03-24 22:59:16 +0100

    Fixed message box

--- client/src/templates/Message.jsx
+++ client/src/templates/Message.jsx
@@ -4,7 +4,7 @@ export default function Message({ title, content }) {
     return (
         <Box mx="auto" p="xl" maw={420}>
             <Text ta="center" fw={800} fz={24} style={{ lineHeight: 1.2 }}>{title}</Text>
-            <Text ta="center" c="gray" mt={4} style={{ lineHeight: 1.2 }}>{content}</Text>
+            <Text ta="center" c="dimmed" mt={8} style={{ lineHeight: 1.2 }}>{content}</Text>
         </Box>
     )
 }
