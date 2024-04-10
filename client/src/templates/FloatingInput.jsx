c4d3cc432b28d130995a8b995d35ed70e23baf98
tomas-sedlak <t.sedlak.24@gmail.com>
2024-04-10 20:32:09 +0200

    Added eye icon to password input

--- client/src/templates/FloatingInput.jsx
+++ client/src/templates/FloatingInput.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import { TextInput, PasswordInput, Loader } from "@mantine/core";
+import { TextInput, PasswordInput, Loader, ActionIcon } from "@mantine/core";
+import { IconEye, IconEyeClosed, IconEyeOff } from "@tabler/icons-react";
 import "css/floatingLabel.css";
-import axios from "axios";
-import { IconEye } from "@tabler/icons-react";
 
 export function FloatingTextInput(props) {
     const [error, setError] = useState(null);
@@ -70,145 +69,10 @@ export function FloatingPasswordInput(props) {
     )
 }
 
-export function RegisterUsernameInput(props) {
-    const [focused, setFocused] = useState(false);
-    const floating = focused || props.inputValue.length > 0 || undefined;
-
-    const handleErrors = async (event) => {
-        if (event.target.value.length === 0) {
-            props.setError(event.target.name, "Toto pole je povinné")
-            return
-        }
-
-        if (!/^[a-zA-Z0-9_]+$/.test(event.target.value)) {
-            props.setError(event.target.name, 'Môže obsahovať iba písmená, čísla a "_"')
-            return
-        }
-
-        const response = await axios.get(`/api/user/unique?username=${event.target.value}`)
-        if (!response.data.unique) {
-            props.setError(event.target.name, "Toto používateľské meno už existuje")
-            return
-        }
-
-        props.setError(event.target.name, null)
-    }
-
-    return (
-        <TextInput
-            classNames={{
-                root: "input-root",
-                label: `input-label ${floating && "floating"} ${focused && "focused"} ${props.error && "error"}`,
-            }}
-            onFocus={() => setFocused(true)}
-            onBlur={async (event) => {
-                setFocused(false)
-                await handleErrors(event)
-            }}
-            onChange={event => {
-                const value = event.target.value
-                if (value.length > 16) return
-                props.setInputValue(event.target.name, event.target.value)
-            }}
-            value={props.inputValue}
-            {...props}
-        />
-    )
-}
-
-export function RegisterEmailInput(props) {
-    const [error, setError] = useState(null);
-    const [focused, setFocused] = useState(false);
-    const floating = focused || props.inputValue.length > 0 || undefined;
-
-    const handleErrors = async (value) => {
-        if (value.length === 0) {
-            setError("Toto pole je povinné")
-            return
-        }
-
-        if (!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/.test(value)) {
-            setError("Neplatný email")
-            return
-        }
-
-        const response = await axios.get(`/api/user/unique?email=${value}`)
-        if (!response.data.unique) {
-            setError("Tento email sa už používa")
-            return
-        }
-
-        setError(null)
-    }
-
-    return (
-        <TextInput
-            type="email"
-            classNames={{
-                root: "input-root",
-                label: `input-label ${floating && "floating"} ${focused && "focused"} ${error && "error"}`,
-            }}
-            onFocus={() => setFocused(true)}
-            onBlur={async (event) => {
-                setFocused(false)
-                await handleErrors(event.target.value)
-            }}
-            onChange={event => {
-                const value = event.target.value
-                if (value.length > 320) return
-                props.setInputValue(event)
-            }}
-            value={props.inputValue}
-            error={error}
-            {...props}
-        />
-    )
-}
-
-export function RegisterPasswordInput(props) {
-    const [error, setError] = useState(null);
-    const [focused, setFocused] = useState(false);
-    const floating = focused || props.inputValue.length > 0 || undefined;
-
-    const handleErrors = (value) => {
-        if (value.length === 0) {
-            setError("Toto pole je povinné")
-            return
-        }
-
-        if (value.length < 8) {
-            setError("Heslo musí mať aspoň 8 znakov")
-            return
-        }
-
-        setError(null)
-    }
-
-    return (
-        <PasswordInput
-            classNames={{
-                root: "input-root",
-                label: `input-label ${floating && "floating"} ${focused && "focused"} ${error && "error"}`,
-            }}
-            onFocus={() => setFocused(true)}
-            onBlur={(event) => {
-                setFocused(false)
-                handleErrors(event.target.value)
-            }}
-            onChange={event => {
-                props.setInputValue(event)
-            }}
-            value={props.inputValue}
-            error={error}
-            {...props}
-        />
-    )
-}
-
-
 export function RegisterInput(props) {
     const { type, validate, setValue, error, status } = props;
     const [focused, setFocused] = useState(false);
+    const [visible, setVisible] = useState(false);
     const floating = focused || props.value.length > 0 || undefined;
 
     return (
@@ -224,9 +88,16 @@ export function RegisterInput(props) {
             }}
             onChange={setValue}
             rightSection={type === "password" ?
-                <IconEye />
-            : status === "loading" && <Loader size={20} />}
+                visible ?
+                    <ActionIcon variant="subtle" color="gray" c="dimmed" onClick={() => setVisible(false)}>
+                        <IconEyeOff stroke={1.25} />
+                    </ActionIcon>
+                    : <ActionIcon variant="subtle" color="gray" c="dimmed" onClick={() => setVisible(true)}>
+                        <IconEye stroke={1.25} />
+                    </ActionIcon>
+                : status === "loading" && <Loader size={20} />}
             {...props}
+            type={type === "password" && visible ? "text" : "password"}
         />
     )
 }
