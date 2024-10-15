7d6ee5a849a1c36ec3257d654ac512f9733e2a5a
tomas-sedlak <t.sedlak.24@gmail.com>
2024-10-15 20:02:50 +0200

    Fixed two loaders on mobile

--- client/src/templates/Aside.jsx
+++ client/src/templates/Aside.jsx
@@ -25,7 +25,7 @@ export default function Aside() {
             <p>Nastala chyba!</p>
         </div>
     ) : (
-        <aside className="aside">
+        <>
             <div className="news-card">
                 <Text px="lg" py="md" fw={700} size="lg" style={{ lineHeight: 1.2 }}>
                     Odporúčaní používatelia
@@ -75,6 +75,6 @@ export default function Aside() {
                     </Link>
                 )}
             </div>
-        </aside>
+        </>
     )
 }
